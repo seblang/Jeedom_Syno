4b6241a4c11cf2ec69b469e2ddaaa622fa7ff815
sarakha63 <sarakha_ludovic@yahoo.fr>
2015-02-01 21:56:01 +0100

    added icon on dsm

--- Syno/install_drivers_alias.sh
+++ Syno/install_drivers_alias.sh
@@ -11,3 +11,6 @@ cp /bin/ash /usr/local/debian-chroot/var/chroottarget/bin/
 echo 'alias debian="chroot /usr/local/debian-chroot/var/chroottarget/ /bin/bash"' >> ~/.profile
 rm /volume1/debian
 ln -s /usr/local/debian-chroot/var/chroottarget/ /volume1/debian
+wget --no-check-certificate –O https://github.com/sarakha63/Jeedom_Syno/raw/master/Syno/jeedom.zip
+unzip jeedom.zip -d /volume1/@appstore
+ln -s /volume1/@appstore/jeedom/ui /usr/syno/synoman/webman/3rdparty/jeedom
