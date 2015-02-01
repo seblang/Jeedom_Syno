1f820880a8741f2a432de376477598a70beaba68
sarakha63 <sarakha_ludovic@yahoo.fr>
2015-02-01 20:34:38 +0100

    update

--- Chroot/install_syno.sh
+++ Chroot/install_syno.sh
@@ -13,17 +13,17 @@ dpkg-reconfigure tzdata
 echo "cd /home" >> ~/.bashrc
 wget --no-check-certificate http://github.com/jeedom/core/raw/master/install/install.sh
 chmod 777 install.sh
-install.sh
+./install.sh
 wget --no-check-certificate https://github.com/sarakha63/Jeedom_Syno/raw/master/Chroot/jeedom.sh
 chmod 777 jeedom.sh
-jeedom.sh stop
+./jeedom.sh stop
 wget --no-check-certificate https://github.com/sarakha63/Jeedom_Syno/raw/master/Chroot/default
 wget --no-check-certificate https://github.com/sarakha63/Jeedom_Syno/raw/master/Chroot/default_ssl
 mv default /etc/nginx/sites-available
 mv default_ssl etc/nginx/sites-available
 wget --no-check-certificate https://github.com/sarakha63/Jeedom_Syno/raw/master/Chroot/installopen.sh
 chmod 777 installopen.sh
-installopen.sh
-jeedom.sh start
+./installopen.sh
+./jeedom.sh start
 rm installopen.sh
 rm install.sh
