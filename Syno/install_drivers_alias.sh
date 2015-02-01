bf4d4ecc1bae31bd07090f82a19d7f48f4831ab4
sarakha63 <sarakha_ludovic@yahoo.fr>
2015-02-01 22:47:16 +0100

    update

--- Syno/install_drivers_alias.sh
+++ Syno/install_drivers_alias.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-wget http://github.com/sarakha63/Jeedom_Syno/raw/master/Syno/cp210x.ko
-wget http://github.com/sarakha63/Jeedom_Syno/raw/master/Syno/ftdi_sio.ko
-wget http://github.com/sarakha63/Jeedom_Syno/raw/master/Syno/pl2303.ko
-wget http://github.com/sarakha63/Jeedom_Syno/raw/master/Syno/usbserial.ko
-wget http://github.com/sarakha63/Jeedom_Syno/raw/master/Syno/S99Modules.sh
 mv *.ko /lib/modules
 mv S99Modules.sh /usr/syno/etc.defaults/rc.d/
 chmod 777 /usr/syno/etc.defaults/rc.d/S99Modules.sh
@@ -11,6 +6,5 @@ cp /bin/ash /usr/local/debian-chroot/var/chroottarget/bin/
 echo 'alias debian="chroot /usr/local/debian-chroot/var/chroottarget/ /bin/bash"' >> ~/.profile
 rm /volume1/debian
 ln -s /usr/local/debian-chroot/var/chroottarget/ /volume1/debian
-wget http://github.com/sarakha63/Jeedom_Syno/raw/master/Syno/jeedom.zip
 unzip jeedom.zip -d /volume1/@appstore
 ln -s /volume1/@appstore/jeedom/ui /usr/syno/synoman/webman/3rdparty/jeedom
